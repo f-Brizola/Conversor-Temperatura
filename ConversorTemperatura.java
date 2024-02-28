24ef1b48316e28d28b18f4a6b99dcfba55e4ab3d
Brizola <160533718+f-Brizola@users.noreply.github.com>
2024-02-27 22:14:23 -0300

    Create ConversorTemperatura.java

---
+++ ConversorTemperatura.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+       
+        int celsius = 0;
+     
+        System.out.println("Informe o valor em Celsius para converter");
+        celsius = scanner.nextInt();
+        
+        double fahrenheit = (celsius * 9/5) + 32;
+        
+        System.out.println("O valor convertido em Fahrenheit eh " + fahrenheit);
+        
+        scanner.close();
+    }
+}
